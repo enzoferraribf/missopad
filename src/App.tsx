9c02cf4c688bc01813c098650c8191b0a94aba65
Enzo Ferrari <ferrarienzobf@gmail.com>
2021-12-28 09:07:48 -0300

    fix: routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route path="/:id" element={<Pad />} />
+        <Route path="/*" element={<Pad />} />
       </Routes>
     </BrowserRouter>
   );
