1b718f8522f6e2e1dad76e7da2c431a6518fe279
Enzo Ferrari <ferrarienzobf@gmail.com>
2023-04-02 12:05:54 -0300

    Add collab support (#22)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { positions, Provider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
 
 import { Home, Pad } from "components";
 
-const options = {
-  timeout: 0,
-  position: positions.BOTTOM_CENTER,
-};
-
 function App() {
   return (
-    <Provider template={AlertTemplate} {...options}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/*" element={<Pad />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Pad />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/utils/time.ts
+++
@@ -1,5 +0,0 @@
-export function lessThan(date: number, seconds: number) {
-  const anHourAgo = Date.now() - seconds;
-
-  return date > anHourAgo;
-}
